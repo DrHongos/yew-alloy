5cf11e94caaa76af02acdf713808a9f98e4e03f1
drhongos <lucasziegenfuhs@gmail.com>
2023-12-14 19:41:41 +0100

    Message creator to sign

--- src/components/button_sign.rs
+++ src/components/button_sign.rs
@@ -8,8 +8,9 @@ use yew::{platform::spawn_local, prelude::*};
 use std::str::FromStr;
 use alloy_signer::Signature;
 use alloy_sol_types::Eip712Domain;
+use web_sys::HtmlInputElement;
 
-fn typed_data_for_document(name: String, chain_id_v: u64, content: String) -> TypedData {
+fn typed_data_for_document(name: &str, chain_id_v: u64, content: &str) -> TypedData {
 
     sol! {
         #[derive(Serialize)]
@@ -20,11 +21,11 @@ fn typed_data_for_document(name: String, chain_id_v: u64, content: String) -> Ty
     };
 
     let doc = DocumentSignature {
-        name: name.clone().into(),
-        content: content,
+        name: name.to_string(),
+        content: content.to_string(),
     };
     let domain_obj: Eip712Domain = alloy_sol_types::eip712_domain!(
-        name: name,
+        name: "AlloYew",
         version: "1",
         chain_id: chain_id_v,
     );
@@ -37,16 +38,22 @@ fn typed_data_for_document(name: String, chain_id_v: u64, content: String) -> Ty
 
 #[function_component(SignatureButton)]
 pub fn signature_button() -> Html {
+    let msg = use_state(|| "Some content to sign".to_string());
+    let name = use_state(|| "Your name".to_string());
+
     let ethereum = use_context::<UseEthereum>().expect(
         "No ethereum found. You must wrap your components in an <EthereumContextProvider />",
     );
     let chain_id = ethereum.chain_id();
+
     let onclick = {
         let ethereum = ethereum.clone();
+        let message = (*msg).clone();
+        let name = (*name).clone();
         Callback::from(move |_: MouseEvent| {
             if ethereum.is_connected() {
-                let data = typed_data_for_document("Rust Dapp".to_string(), chain_id, "Some content to sign".to_string());
-                //log(format!("data: {:#?}",data).as_str());                
+                let data = typed_data_for_document(&name, chain_id, &message);
+                //log(format!("data: {:#?}",data).as_str());
                 let ethereum = ethereum.clone();
                 
                 spawn_local(async move {
@@ -75,9 +82,32 @@ pub fn signature_button() -> Html {
             }
         })
     };
+
+    let on_change_message = {
+        let message = msg.clone();
+        Callback::from(move |e: Event| {
+            let input: HtmlInputElement = e.target_unchecked_into();
+            message.set(input.value());
+        })
+    };
+    let on_change_name = {
+        let naming = name.clone();
+        Callback::from(move |e: Event| {
+            let input: HtmlInputElement = e.target_unchecked_into();
+            naming.set(input.value());
+        })
+    };
+
     html! {
-        <>
-        <button {onclick} class={"button"} disabled={!ethereum.is_connected()}>{"Test signature"}</button>
-        </>
+        <div class={"signer"}>
+        
+        <label>{"Name"}</label>
+        <input onchange={on_change_name} class={"name_input"} disabled={!ethereum.is_connected()} type="text" value={(*name).clone()} />
+        <br />
+        <label>{"Content"}</label>
+        <input onchange={on_change_message} class={"msg_input"} disabled={!ethereum.is_connected()} type="text" value={(*msg).clone()} />
+        <hr />
+        <button {onclick} class={"button"} disabled={!ethereum.is_connected()}>{"Sign it"}</button>
+        </div>
     }
 }
